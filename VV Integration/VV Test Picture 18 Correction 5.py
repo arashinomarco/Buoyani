63f579e27736ced778f4251f7dfd2ab1812caf9f
cleaning materials lang maiiambag ko sa klase <59410269+arashinomarco@users.noreply.github.com>
2024-03-11 13:10:46 +0800

    Create VV Test Picture 18 Correction 5.py

---
+++ VV Integration/VV Test Picture 18 Correction 5.py
@@ -0,0 +1,6 @@
+def update_acceleration(particles, yellow_dot_position):
+    for p in particles:
+        acceleration_direction = yellow_dot_position - p.position
+        acceleration_magnitude = 30  # kph
+        acceleration = (acceleration_direction / np.linalg.norm(acceleration_direction)) * (acceleration_magnitude / 3.6) * dt
+        p.acceleration += acceleration
